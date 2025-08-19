b6c95ec0a2d9899126ec8d7edf06ee7716dc2057
vladkuznetsov2901 <vladkuznetsov2901@gmail.com>
2025-08-19 12:56:05 +0300

    network api add + models

--- app/src/main/java/com/example/weatherapptest/data/Api.kt
+++ app/src/main/java/com/example/weatherapptest/data/Api.kt
@@ -1,5 +1,7 @@
 package com.example.weatherapptest.data
 
+import com.example.weatherapptest.data.models.CurrentWeatherDto
+import com.example.weatherapptest.data.models.ForecastDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
